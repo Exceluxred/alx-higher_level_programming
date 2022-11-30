c9bab29b8a57292d05ce5c83280763786fc40951
Exceluxred <excelartmusic@gmail.com>
2022-11-30 20:59:44 +0000

    Fix indentation error

--- 0x01-python-if_else_loops_functions/102-magic_calculation.py
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -1,6 +1,3 @@
-c_calculation(a, b, c):
-    if a < b:
-        return c
-    elif c > b:
-        return a + b
-    return a * b - c
+#!/usr/bin/python3
+def magic_calculation(a, b):
+    return 98 + a ** b
