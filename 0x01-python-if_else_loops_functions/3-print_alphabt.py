042a55d11d0cb28583134d7d364b30f25a09d417
Exceluxred <excelartmusic@gmail.com>
2022-11-30 20:47:49 +0000

    Print string with a variable number

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
-
-for i in range(97, 123):
-    if i == 101 or i == 113:
-        continue
-    print("{:c}".format(i), end='')
+number = 98
+print("{:d} Battery street".format(number))
