084bf619912c17149d50841c8f77ba785568aaf2
Exceluxred <excelartmusic@gmail.com>
2022-12-08 00:20:03 +0000

    hot fix

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
-def best_score(a_dictionary={}):
+def best_score(a_dictionary):
     if a_dictionary is None:
-        return
+        return None
+
+    if len(a_dictionary.values()) == 0:
+        return None
+
+    max_value = max(a_dictionary.values())
+
     for key, value in a_dictionary.items():
-        if value == max(a_dictionary.values()):
-            return 
+        if value == max_value:
+            return key
