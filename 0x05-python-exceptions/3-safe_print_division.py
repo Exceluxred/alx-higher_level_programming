eae3814d280688ab4cc09ed4115603b52e7a7624
Exceluxred <excelartmusic@gmail.com>
2022-12-21 15:34:03 +0100

    Print division, None if error

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,13 +1,9 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
     try:
-        value = a / b
-    except:
-        pass
+        div = a / b
+    except ZeroDivisionError:
+        div = None
     finally:
-        try:
-            print("Inside result: {}".format(value))
-        except:
-            print("Inside result: {}".format(None))
-            return None
-    return value
+        print("Inside result: {}".format(div))
+    return div
