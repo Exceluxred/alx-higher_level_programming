dd60b19863851540bb663063bdb40a2a842c5c0e
Exceluxred <excelartmusic@gmail.com>
2022-12-21 14:52:35 +0100

    Divide each element of one list by each element of another list

---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    div_list = []
+    for i in range(list_length):
+        div_list.append(0)
+        try:
+            div_list[i] = my_list_1[i] / my_list_2[i]
+        except ZeroDivisionError:
+            print("division by 0")
+        except TypeError:
+            print("wrong type")
+        except IndexError:
+            print("out of range")
+        finally:
+            pass
+    return div_list
